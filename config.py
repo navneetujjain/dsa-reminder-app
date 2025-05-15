4a82491c1d1a992e104b653def6c962053e29a76
Navneet Ujjain <navneetujjain27@gmail.com>
2025-05-15 13:42:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'dsa_reminder.db')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'dsa_reminder.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
     SENDER_EMAIL = os.getenv('SENDER_EMAIL', 'your-email@example.com')
